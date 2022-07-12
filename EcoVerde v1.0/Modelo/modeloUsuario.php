fa26312a06b291c829dc6106ea4709bb8f377df1
LeonardoCuna <107080688+LeonardoCuna@users.noreply.github.com>
2022-07-12 02:17:42 -0300

    CRUD usuario sin terminar

--- EcoVerde v1.0/Modelo/modeloUsuario.php
+++ EcoVerde v1.0/Modelo/modeloUsuario.php
@@ -123,6 +123,76 @@ Class Usuario{
         }
     }
 
+    public function RegistrarUsuarios($Cedula, $Nombre, $Apellido, $Celular, $Email, $Clave, $Calle, $Numero, $Esquina, $Barrio, $Tipo){
+        $Estado= "Aceptado";
+        $sql = "INSERT INTO usuario VALUES ('$Cedula', NULL, '$Nombre', '$Apellido', '$Celular', '$Email', '$Clave', '$Calle', '$Numero', '$Esquina', '$Barrio', '$Tipo', '$Estado')";
+        
+        
+        if($this->db->query($sql)){
+            return true;
+            
+        }else{
+            return false;
+        }
+    }
+
+    public function EliminarUsuario($c){
+	
+        $sql = "DELETE FROM usuario WHERE CI = '$c'";
+        if($this->db->query($sql)){
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+
+    public function ModificarUsuarios($c, $nombre, $apellido, $celular, $email, $calle, $numero, $esquina, $barrio, $tipo){
+	
+        $sql = "UPDATE usuario SET nombre = '$nombre' WHERE CI = '$c'";
+        $sql1 = "UPDATE usuario SET apellido = '$apellido' WHERE CI = '$c'";
+        $sql2 = "UPDATE usuario SET celular = '$celular' WHERE CI = '$c'";
+        $sql3 = "UPDATE usuario SET email = '$email' WHERE CI = '$c'";
+        $sql4 = "UPDATE usuario SET calle = '$calle' WHERE CI = '$c'";
+        $sql5 = "UPDATE usuario SET numero = '$numero' WHERE CI = '$c'";
+        $sql6 = "UPDATE usuario SET esquina = '$esquina' WHERE CI = '$c'";
+        $sql7 = "UPDATE usuario SET barrio = '$barrio' WHERE CI = '$c'";
+        $sql8 = "UPDATE usuario SET tipo = '$tipo' WHERE CI = '$c'";
+        if($nombre!=""){
+            $modificar = $this->db->query($sql);
+        }
+        if($apellido!=""){
+            $modificar = $this->db->query($sql1);
+            }
+
+        if($celular!=""){
+            $modificar = $this->db->query($sql2);
+        }
+        
+        if($email!=""){
+            $modificar = $this->db->query($sql3);
+            }
+        
+        if($calle!=""){
+            $modificar = $this->db->query($sql4);
+        }
+        if($numero!=""){
+            $modificar = $this->db->query($sql5);
+            } 
+
+        if($esquina!=""){
+            $modificar = $this->db->query($sql6);
+        }
+        
+        if($barrio!=""){
+            $modificar = $this->db->query($sql7);
+        }
+
+        if($tipo!=""){
+            $modificar = $this->db->query($sql8);
+        }
+    }	
+
 
     
 
